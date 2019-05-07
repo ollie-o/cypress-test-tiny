0ce64ef1afe99a074e1e1e702c11aa7e502aed74
Ollie O'Donnell <Oliver.O@ibm.com>
2019-05-06 23:56:47 -0500

    feat: repro

--- cypress/integration/spec.js
+++ cypress/integration/spec.js
@@ -1,3 +1,6 @@
-describe('page', () => {
-  it('works', () => {})
-})
+describe("page", () => {
+  it("works", () => {
+    cy.task("log", "This should only be logged in the terminal once!");
+    cy.visit("https://google.com");
+  });
+});
